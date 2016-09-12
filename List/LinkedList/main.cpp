bbaa84d092fc600ed9f47c8c4ed3b3178313a1f3
Evan Laferriere <laferriere_evan@wheatoncollege.edu>
2016-09-12 16:52:00 -0400

    Started LinkedList

---
+++ List/LinkedList/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+class Node
+{
+    int num;
+    Node* next;
+};
+
+class LinkedList
+{
+    int hmr_;
+    Node* head_, tail_;
+    void create_list(Node* node_pointer);
+    void add(int where, int what);
+};
+void createList::LinkedList(Node* node)
+{
+
+}
+int main()
+{
+
+}
