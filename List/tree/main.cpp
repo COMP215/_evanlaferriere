8a0fd86da9ec305e18559bfbb9db05ff43a2c083
Evan Laferriere <laferriere_evan@wheatoncollege.edu>
2016-09-14 16:28:13 -0400

    Tree functionality on nigh

--- List/tree/main.cpp
+++ List/tree/main.cpp
@@ -8,10 +8,14 @@ class Node
         Node* left = NULL;
         Node* right = NULL;
         Node* root = NULL;
-        Node();
+        Node(int data);
         Node(Node* left, Node* right);
         Node(int num, Node* left, Node* right);
 };
+Node::Node(int data)
+{
+    num = data;
+}
 Node::Node(Node* left, Node* right)
 {
     this->left = left;
@@ -25,27 +29,60 @@ Node::Node(int num, Node* left, Node* right)
 }
 class Tree
 {
-    int hmr_data_ = 0;
-    int* data_ = new int[hmr_data_];
-    Node* buffer_ = new Node(int* );
-    Tree(int* passed_data, int num_ints);
+    public:
+        int hmr_data_ = 0;
+        int* data_ = new int[hmr_data_];
+        Node* buffer_ = new Node(NULL);
+        Tree(int* passed_data);
+        void Print(Node* printing_node);
+        Node* Print_This();
 };
-Tree::Tree(int* passed_data, int num_ints)
+Tree::Tree(int* passed_data)
 {
-    hmr_data_=num_ints;
+    hmr_data_= sizeof(passed_data)/sizeof(hmr_data_);
     data_= passed_data;
     int num = 1;
-    buffer_->left = new Node(data_[0])
-    Node* sorting_node = buffer_;
+    buffer_->root = new Node(data_[0]);
+    Node* sorting_node = buffer_->root;
     while(num < hmr_data_)
     {
-        if
+        if(sorting_node->num < data_[num])
+        {
+            sorting_node->right = new Node(data_[num]);
+            num++;
+            sorting_node = sorting_node->right;
+        }
+        if(sorting_node->num > data_[num])
+        {
+            sorting_node->left = new Node(data_[num]);
+            num++;
+            sorting_node = sorting_node->left;
+        }
+        while(sorting_node->root->num > data_[num])
+        {
+            sorting_node = sorting_node->root;
+        }
     }
 }
+void Tree::Print(Node* printing_node)
+{
+    if(printing_node->left == NULL && printing_node->right == NULL)
+    {
+        cout << "if'd" << endl;
+        return;
+    }
+    Print(printing_node->left);
+    cout << printing_node->num;
+    Print(printing_node->right);
+}
+Node* Tree::Print_This()
+{
+    return buffer_->root;
+}
+
 int main()
 {
-    int foo[5] = {1,2,3,4,5};
-    Tree bar(foo, 5);
-
-
+    int foo[8] = {17,34,9,16,10,1,74,20};
+    Tree bar(foo);
+    bar.Print(bar.Print_This());
 }
---
+++ List/tree/tree.h
@@ -0,0 +1,5 @@
+#ifndef tree_h
+#define tree_h
+
+
+#endif // tree_h
