23b3afff4a85abc05a48bdf5ffb25b664270a3b5
Evan Laferriere <laferriere_evan@wheatoncollege.edu>
2016-09-19 14:02:11 -0400

    Finished tree, not printing right

--- List/tree/main.cpp
+++ List/tree/main.cpp
@@ -1,88 +1,100 @@
 #include <iostream>
 
+
 using namespace std;
 class Node
 {
     public:
-        int num;
-        Node* left = NULL;
-        Node* right = NULL;
-        Node* root = NULL;
+        int num_ = NULL;
+        Node* left_ = NULL;
+        Node* right_ = NULL;
+        Node* root_ = NULL;
         Node(int data);
         Node(Node* left, Node* right);
         Node(int num, Node* left, Node* right);
 };
 Node::Node(int data)
 {
-    num = data;
+    num_ = data;
 }
 Node::Node(Node* left, Node* right)
 {
-    this->left = left;
-    this->right = right;
+    this->left_ = left;
+    this->right_ = right;
 }
 Node::Node(int num, Node* left, Node* right)
 {
-    this->num = num;
-    this->left = left;
-    this->right = right;
+    this->num_ = num;
+    this->left_ = left;
+    this->right_ = right;
 }
 class Tree
 {
     public:
         int hmr_data_ = 0;
-        int* data_ = new int[hmr_data_];
-        Node* buffer_ = new Node(NULL);
-        Tree(int* passed_data);
+        int total_ = 0;
+        int* data_ = NULL;
+        Node* buffer_ = new Node(32767);
+        Tree(int passed_data[], int total);
+        void Add(Node* sorting_node);
         void Print(Node* printing_node);
-        Node* Print_This();
 };
-Tree::Tree(int* passed_data)
+Tree::Tree(int* passed_data, int total)
 {
-    hmr_data_= sizeof(passed_data)/sizeof(hmr_data_);
+    total_ = total;
     data_= passed_data;
-    int num = 1;
-    buffer_->root = new Node(data_[0]);
-    Node* sorting_node = buffer_->root;
-    while(num < hmr_data_)
-    {
-        if(sorting_node->num < data_[num])
-        {
-            sorting_node->right = new Node(data_[num]);
-            num++;
-            sorting_node = sorting_node->right;
-        }
-        if(sorting_node->num > data_[num])
-        {
-            sorting_node->left = new Node(data_[num]);
-            num++;
-            sorting_node = sorting_node->left;
-        }
-        while(sorting_node->root->num > data_[num])
-        {
-            sorting_node = sorting_node->root;
-        }
-    }
+    hmr_data_ = 0;
+    buffer_->root_ = new Node(data_[hmr_data_]);
+    buffer_->root_->root_ = buffer_;
+    hmr_data_++;
+    this->Add(buffer_->root_);
+    this->Print(buffer_->root_);
 }
 void Tree::Print(Node* printing_node)
 {
-    if(printing_node->left == NULL && printing_node->right == NULL)
-    {
-        cout << "if'd" << endl;
-        return;
-    }
-    Print(printing_node->left);
-    cout << printing_node->num;
-    Print(printing_node->right);
+    if(printing_node->left_ != NULL)
+        Print(printing_node->left_);
+    cout << printing_node->num_ << ' ';
+    if(printing_node->right_ != NULL)
+        Print(printing_node->right_);
+    return;
 }
-Node* Tree::Print_This()
+void Tree::Add(Node* sorting_node)
 {
-    return buffer_->root;
+    if(hmr_data_ < total_)
+    {
+        if(sorting_node->num_ == NULL)
+        {
+            sorting_node->num_ = data_[hmr_data_];
+            hmr_data_++;
+            Add(sorting_node);
+            return;
+        }
+        if(sorting_node->root_->num_ <= data_[hmr_data_])
+        {
+            Add(sorting_node->root_);
+            return;
+        }
+        if(sorting_node->num_ < data_[hmr_data_])
+        {
+            sorting_node->right_ = new Node(NULL);
+            sorting_node->right_->root_ = sorting_node;
+            Add(sorting_node->right_);
+            return;
+        }
+        if(sorting_node->num_ > data_[hmr_data_])
+        {
+            sorting_node->left_ = new Node(NULL);
+            sorting_node->left_->root_ = sorting_node;
+            Add(sorting_node->left_);
+            return;
+        }
+    }
+    return;
 }
-
 int main()
 {
     int foo[8] = {17,34,9,16,10,1,74,20};
-    Tree bar(foo);
-    bar.Print(bar.Print_This());
+    Tree bar(foo, 8);
+    return 0;
 }
